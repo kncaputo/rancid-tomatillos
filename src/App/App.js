8ea2f966abc2bc45b104bc7fe675167cbaa0d94d
Kara Caputo <kncaputo@gmail.com>
2020-12-04 09:01:34 -0500

    Add back button

--- src/App/App.js
+++ src/App/App.js
@@ -6,35 +6,47 @@ import movieData from '../data.js';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: movieData.movies,
-      movie: {}
-    }
-  }
+	constructor() {
+		super();
+		this.state = {
+			movies: movieData.movies,
+			movie: {}
+		}
+	}
 
-  getSingleMovie = (movieId) => {
-    const clickedMovie = {"movie": 
-    {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-    backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-    release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-    average_rating: 6, genres: [{id: 18, name:"Drama"}], budget:63000000, 
-    revenue:100853753, runtime:139, tagline: "It's a movie!" }}
+	getSingleMovie = (movieId) => {
+		const clickedMovie = {"movie": 
+		{id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
+		backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
+		release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
+		average_rating: 6, genres: [{id: 18, name:"Drama"}], budget:63000000, 
+		revenue:100853753, runtime:139, tagline: "It's a movie!" }}
 
-    this.setState({movie: clickedMovie})
-  }
+		this.setState({movie: clickedMovie})
+	}
+
+	goHome = () => {
+		this.setState({movie: {}})
+	}
 
 
-  render() {
-    return (
-      <main className="App">
-        <Header />
-        {Object.keys(this.state.movie).length === 0 && <MovieContainer movies={this.state.movies} getSingleMovie={this.getSingleMovie}/>}
-        {Object.keys(this.state.movie).length > 0 && <MovieDetails movie={this.state.movie}/>}
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main className="App">
+				<Header />
+				<header>
+					<h1>Rancid Tomatillos</h1>
+					<nav>
+						{Object.keys(this.state.movie).length > 0 && <button onClick={() => {this.goHome()}}>Back</button>}
+					</nav>
+				</header>
+				
+	 
+				{Object.keys(this.state.movie).length === 0 && <MovieContainer movies={this.state.movies} getSingleMovie={this.getSingleMovie}/>}
+				{Object.keys(this.state.movie).length > 0 && <MovieDetails movie={this.state.movie}/>}
+			</main>
+		);
+	}
 }
 
 export default App;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,6 +4,9 @@ const Header = () => {
   return (
     <header>
       <h1>Rancid Tomatillos</h1>
+      <nav>
+        <button>Back</button>
+      </nav>
     </header>
   )
 }
