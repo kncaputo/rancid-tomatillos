7666966da0f98dfe33aeb7bf9c1deb34218be8ce
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-12-05 15:13:31 -0700

    Add MovieDetails tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { fetchSingleMovie, fetchMovies } from '../apiCalls.js'
 import App from './App';
+
+it('should display correct movie details based on id', () => {
+		
+})
--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './MovieDetails.css';
 
 const MovieDetails = ({ movie }) => {
-  // console.log(movie.genre[0])
   return (
     <section>
       <img className='card-img' src={movie.backdrop_path} />
@@ -10,6 +9,7 @@ const MovieDetails = ({ movie }) => {
       <p>{movie.overview}</p>
       <p>Average User Rating: {movie.average_rating}</p>
       <p>Genre: {movie.genres[0]}</p>
+			<p>Release Date: {movie.release_date}</p>
       <p>Budget: ${movie.budget}</p>
       <p>Total Revenue: ${movie.revenue}</p>
       <p>Runtime: {movie.runtime}</p>
