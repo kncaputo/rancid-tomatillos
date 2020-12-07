4678f628e45f12cc6caed3294ce96d1e1fa459ab
Kara Caputo <kncaputo@gmail.com>
2020-12-07 18:36:27 -0500

    Add styles to center movie details

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -5,7 +5,11 @@ const MovieDetails = ({ movie, statusCode, error }) => {
 
 	const formatNum = (num, type) => {
 		return (
-			num > 0 && <p><b>{ type }:</b> ${new Intl.NumberFormat('en-US').format(num)}</p>
+      num > 0 && 
+      <section className='details-box'>
+        <p className='l-details'><b> {type} :</b></p>
+        <p className='r-details'>${new Intl.NumberFormat('en-US').format(num)}</p>
+      </section>
 		)
 	}
 
@@ -14,15 +18,29 @@ const MovieDetails = ({ movie, statusCode, error }) => {
       {statusCode < 400 ? 
 				<section className='movie-details-box'>
 					<img className='card-img' src={movie.backdrop_path} />
-					<h1><b>{movie.title}</b></h1>
-
-					<div className='overview'><p><b>Overview:</b><br/>{movie.overview}</p>
-					<p><b>Average User Rating:</b> {movie.average_rating.toFixed(2)}</p></div>
-					<p><b>Genre:</b> {movie.genres[0]}</p>
-					<p><b>Release Date:</b> {movie.release_date}</p>
-					{ formatNum(movie.budget, 'Budget') }
+					<h1 className='movie-title'><b>{movie.title}</b></h1>
+          <section className='details-box'>
+              <p className='l-details'><b>Average User Rating:</b></p>
+              <p className='r-details'>{movie.average_rating.toFixed(2)}</p>
+          </section>
+					<section className='overview-box'>
+            <p className='overview-title'><b>Overview:</b></p>
+            <p className='overview-text'>{movie.overview}</p>
+          </section>
+          <section className='details-box'>
+            <p className='l-details'><b>Genre:</b></p>
+            <p className='r-details'>{movie.genres[0]}</p>
+          </section>
+          <section className='details-box'>
+            <p className='l-details'><b>Release Date:</b></p>
+            <p className='r-details'>{movie.release_date}</p>
+          </section>
+          { formatNum(movie.budget, 'Budget') }
 					{ formatNum(movie.revenue, 'Total Revenue') }
-					<p><b>Runtime:</b> {movie.runtime} minutes</p>
+          <section className='details-box'>
+            <p className='l-details'><b>Runtime:</b></p>
+            <p className='r-details'>{movie.runtime} minutes</p>
+          </section>
 				</section> :
 				<section>
 					<h1>Whoops, it looks like something went wrong!</h1>
