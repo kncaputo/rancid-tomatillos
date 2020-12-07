29a19546857b7ce73f9cc8d3be5e8d3d9b9942a1
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-12-07 15:01:22 -0700

    Add formatNums function to convert budget and revenue

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -2,23 +2,30 @@ import React from 'react';
 import './MovieDetails.css';
 
 const MovieDetails = ({ movie, statusCode, error }) => {
+
+	const formatNum = (num, type) => {
+		return (
+			num > 0 && <p><b>{ type }:</b> ${new Intl.NumberFormat('en-US').format(num)}</p>
+		)
+	}
+
   return (
     <section>
       {statusCode < 400 ? 
 				<section>
 					<img className='card-img' src={movie.backdrop_path} />
-					<h1>{movie.title}</h1>
-					<p>{movie.overview}</p>
-					<p>Average User Rating: {movie.average_rating}</p>
-					<p>Genre: {movie.genres[0]}</p>
-					<p>Release Date: {movie.release_date}</p>
-					<p>Budget: ${movie.budget}</p>
-					<p>Total Revenue: ${movie.revenue}</p>
-					<p>Runtime: {movie.runtime} minutes</p>
+					<h1><b>{movie.title}</b></h1>
+					<p><b>Overview:</b> <br/> {movie.overview}</p>
+					<p><b>Average User Rating:</b> {movie.average_rating.toFixed(2)}</p>
+					<p><b>Genre:</b> {movie.genres[0]}</p>
+					<p><b>Release Date:</b> {movie.release_date}</p>
+					{ formatNum(movie.budget, 'Budget') }
+					{ formatNum(movie.revenue, 'Total Revenue') }
+					<p><b>Runtime:</b> {movie.runtime} minutes</p>
 				</section> :
 				<section>
 					<h1>Whoops, it looks like something went wrong!</h1>
-					<p>{statusCode} {errror}</p>
+					<p>{statusCode} {error}</p>
 				</section>
 			}
     </section>
